b0868bb5601c0102b37595005c2cb39aa9880f81
Aaron Langley <alangley@gmail.com>
2022-06-24 15:10:32 -0400

    Finished NK1 segment

--- adt_generate.py
+++ adt_generate.py
@@ -5,7 +5,8 @@ import random
 import string
 
 #dictionaries to pull data from for messages
-   #might be moved later on to a db or separate files 
+   #might be moved later on to a db or separate files
+################################################################################
 sex = {'M','F'}
 marital = {'M','S',}
 last_names = {'Muir','Smith','Adams','Garland','Meade','Fitzgerald','White','Weir','Johnson','Dalton','Reed','Black','Greene','Nedemyer'}
@@ -15,7 +16,7 @@ race = {'AI','NH','B','U','W','A','O'}
 street = {'Ford St.','Sunshine Lane','Seasame St.','Main St.','Delphi Cres.', 'Miller Ln.', 'Younge St.', 'Main Rd.', 'First Ave', 'Oak St.'}
 city = {'Smithville','Culver City','Johnstown','Blundersburg','New City','Smallsville','Cedar Springs','Dogwood','Spring Valley','Pine Hill'}
 state= {'CA','OR','WA','ID','NV','MN','AZ','NM','WY','CO'}
-relation = {'Grandchild', 'Second Cousin', 'Sibling', 'Parent'}
+relation = {'Grandchild','Second Cousin','First Cousin','Sibling','Parent','Guardian','Uncle','Aunt','Spouse'}
 adt_event = {'A01', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08'}
 facility = {'Lab','ER','Clinic','Oncology','MedSurg','ICU'}
 application = {'Epic', 'Allscripts', 'Cerner','Expanse','Magic','CPSI','eCW'}
@@ -43,7 +44,7 @@ def createMSH(event,time,application):
    for x in range(1,len(MSH)):
       MSH_string += (MSH[x] + "|")
 
-   return(print("MSH" + MSH_string))
+   return(print("MSH|" + MSH_string))
 
 #create EVN segment
 def createEVN(event,time,reason, id, event_time, event_facility):
@@ -92,144 +93,28 @@ def createPID(sex,last,first,middle,dob,race,address,phone,marital):
 
    return(print("PID" + PID_string))
 
+#creates NK1 segment
+def createNK1(last,first,middle,relation,address,phone):
+   NK1=[]
+   NK1_string=""
+   NK1.insert(1,"1")
+   NK1.insert(2,"%s^%s^%s"%(last,first,middle))
+   NK1.insert(3,relation)
+   NK1.insert(4,address)
+   NK1.insert(5,phone)
+   NK1.insert(6,"")
+   NK1.insert(7,"")
+   NK1.insert(8,"")
+   NK1.insert(9,"")
+   
+   for x in range(1,len(NK1)):
+      NK1_string += (NK1[x] + "|")
+
+   return(print("NK1|" + NK1_string))
 """
 PV1|1|E|ER^^^.|EM|||1396088571^KENNEDY^DEREK^^^^MD|||||||EME||Y|||35001346|MCR|||||||||||||||||||.|||||202206232008||||||||1649274275^KERRIGAN^BRIAN^^^^DO
+"""
 
-"""
-"""
- 
-function ran.PV1(PV1)
-   PV1[8][1][2][1] = ran.lastName()
-   PV1[8][1][3] = ran.firstName()
-   PV1[8][1][4] = 'F'
-   PV1[19][1] = math.random(9999999)
-   PV1[44][1] = ran.TimeStamp()
-   PV1:S()
-end
- 
-function ran.NK1(NK1)
-   for i = 1, math.random(6) do
-      NK1[i][1] = i
-      ran.Kin(NK1[i])
-   end
-end
- 
-function ran.Kin(NK1)
-   NK1[2][1][1][1] = ran.lastName()  
-   NK1[2][1][2] = ran.firstName()
-   NK1[3][1] = ran.choose(ran.Relation)
-end
- 
-
- 
-function ran.lastName() return ran.choose(ran.LastNames) end
- 
-function ran.choose(T)
-   return T[math.random(#T)]
-end
- 
-function ran.NameAndSex(PID)
-   if math.random(2) == 1 then
-      PID[8] = 'M'
-      PID[5][1][2] = ran.choose(ran.MaleNames)
-   else   
-      PID[8] = 'F'
-      PID[5][1][2] = ran.choose(ran.FemaleNames)      
-   end
-end
- 
-function ran.firstName()
-   if math.random(2) == 1 then
-      return ran.choose(ran.MaleNames)
-   else   
-      return ran.choose(ran.FemaleNames)      
-   end
-end
- 
-function ran.Date()
-   local T = os.date('*t')
-  
-   local newDate = '19'..rand(T.year,99,2)..
-   rand(T.month,12,2)..
-   rand(T.day,29,2)
-   
-   return newDate
-end
- 
-function ran.TimeStamp()
-   local T = os.date('*t')
-   
-   local newDate = '20'..rand(T.year,tostring(T.year):sub(-2),2)..
-   rand(T.month,12,2)..
-   rand(T.day,29,2)..
-   rand(T.hour,12,2)..
-   rand(T.min,60,2)..
-   rand(T.sec,60,2)
-   
-   return newDate
-end
- 
-function ran.AcctNo()
-   return math.random(99)..'-'..math.random(999)..'-'..math.random(999)
-end
- 
-ran.Locations = { {'Chicago', 'IL'}, {'Toronto', 'ON'}, {'ST. LOUIS', 'MO'}, {'LA', 'CA'} }
- 
-function ran.location()
-   local R = ran.choose(ran.Locations)
-   return R[1], R[2]
-end
- 
-function ran.SSN()
-   return math.random(999)..'-'
-          ..math.random(999)..'-'
-          ..math.random(999)
-end
- 
-function ran.addWeight(Out)
-   local OBX = Out.OBX[#Out.OBX+1]
-   OBX[3][1] = 'WT'
-   OBX[3][2] = 'WEIGHT'
-   OBX[5][1][1] = math.random(100) + 30
-   OBX[6][1] = 'pounds'
-   return OBX
-end
-   
-function ran.addHeight(Out)
-   local OBX = Out.OBX[#Out.OBX+1]
-   OBX[3][1] = 'HT'
-   OBX[3][2] = 'HEIGHT'
-   OBX[5][1][1] = math.random(100) + 20
-   OBX[6][1] = 'cm'
-   return OBX
-end
- 
-function ran.RandomMessage()
-   local Out = hl7.message{vmd='example/demo.vmd', name='ADT'} 
-   ran.scrubMSH(Out.MSH)
-   ran.scrubEVN(Out.EVN)
-   ran.scrubPID(Out.PID)
-   ran.PV1(Out.PV1)
-   ran.NK1(Out.NK1)
-   ran.addWeight(Out)
-   ran.addHeight(Out)
-   return Out:S()   
-end
- 
-function rand(In, Max, Size)
-   
-   local Result = tostring((In + math.random(Max)) % Max)
-   if '0' == Result then
-      Result = '1'
-   end
-   
-   while Size > Result:len() do
-      Result = '0'..Result
-   end
-   
-   return Result
-end
-"""
 #segemnt function inputs
 message_event = random.choice(tuple(adt_event))
 message_time = str(datetime.now().strftime("%Y%m%d%H%M%S"))
@@ -276,9 +161,38 @@ def createPatient():
 #create patient details
 patient_sex,patient_last,patient_first,patient_middle,patient_dob,patient_race,patient_address,patient_phone,patient_marital = createPatient()
 
+#create cohesive nk data in line with patient
+def createNextofKin(patient_last):
+   nk_street=str(
+      random.randrange(1,9999,1)) + " " +random.choice(tuple(street)
+   )#not returned
+   nk_city=random.choice(tuple(city))#not returned
+   nk_state=random.choice(tuple(state))#not returned
+   nk_zip=str(random.randrange(10000,99999,1))#not returned
+   nk_address="%s^^%s^%s^%s"%(
+      nk_street,nk_city,nk_state,nk_zip
+   )
+   nk_sex = random.choice(tuple(sex))#not returned
+   nk_last=patient_last
+
+   if nk_sex == 'M':
+      nk_first=random.choice(tuple(male_names))
+   else:
+      nk_first=random.choice(tuple(female_names))
+   
+   nk_middle=random.choice(string.ascii_uppercase)
+   nk_relation=random.choice(tuple(relation))
+   nk_phone="(" +str(random.randrange(100,999,1))+")555-"+str(random.randrange(1000,9999,1))
+
+   return nk_address,nk_first,nk_last,nk_middle,nk_relation,nk_phone
+
+#create nk details
+nk_address,nk_first,nk_last,nk_middle,nk_relation,nk_phone = createNextofKin(patient_last)
+
 #call functions to print the segments
 createMSH(message_event,message_time,message_application)
 createEVN(message_event,message_time,message_reason,event_time,message_clerk,event_facility
 )
 createPID(patient_sex,patient_last,patient_first,patient_middle,patient_dob,patient_race,patient_address,patient_phone,patient_marital
 )
+createNK1(nk_last,nk_first,nk_middle,nk_relation,nk_address,nk_phone)
