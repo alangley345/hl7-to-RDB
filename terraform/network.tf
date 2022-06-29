6a219867712ce11118892f046593d85d8b6fd5fc
Aaron Langley <alangley345@gmail.com>
2022-06-29 16:06:10 -0400

    added vpc definition

---
+++ terraform/network.tf
@@ -0,0 +1,10 @@
+#Subnet1 10.40.100.0/24
+resource "aws_subnet" "mirth" {
+  vpc_id            = data.terraform_remote_state.base_state.outputs.production_vpc
+  cidr_block        = "10.40.100.0/24"
+  depends_on        = [data.terraform_remote_state.base_state.outputs.production_vpc]
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Mirth Instance"
+  }
+}
