7f3807c19687347fae68712bff106f43c40cbc55
Aaron Langley <alangley@gmail.com>
2022-06-28 21:49:04 -0400

    Adding role for ecs

--- terraform/ecs_mirth.tf
+++ terraform/ecs_mirth.tf
@@ -6,3 +6,28 @@ resource "aws_ecs_cluster" "mirth" {
     value = "enabled"
   }
 }
+
+resource "aws_ecs_service" "mirth" {
+  name            = "mirth_engine"
+  cluster         = aws_ecs_cluster.mirth.id
+  task_definition = aws_ecs_task_definition.mirth.arn
+  desired_count   = 1
+  iam_role        = aws_iam_role.foo.arn
+  depends_on      = [aws_iam_role_policy.foo]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.foo.arn
+    container_name   = "mongo"
+    container_port   = 8080
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
---
+++ terraform/iam_role_mirth.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "mirth-instance" {
+  name = "Mirth_Service"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
