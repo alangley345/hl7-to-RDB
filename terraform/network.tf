7c8e5609f76a58a44b29e7a5b400021e8e39a08a
Aaron Langley <alangley345@gmail.com>
2022-06-29 22:38:48 -0400

    Trying to ad dvpc components

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,6 +1,6 @@
 #Subnet1 10.40.100.0/24
 resource "aws_subnet" "mirth" {
-  vpc_id            = data.terraform_remote_state.base_state.outputs.production_vpc
+  vpc_id            = data.terraform_remote_state.base_state.outputs.production_vpc_id
   cidr_block        = "10.40.100.0/24"
   depends_on        = [data.terraform_remote_state.base_state]
   availability_zone = "us-east-1a"
