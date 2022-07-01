d867031df1ee29a380529888e2720690ddc40413
Aaron Langley <alangley@gmail.com>
2022-07-01 11:35:25 -0400

    removed contraints

--- terraform/ecs_mirth.tf
+++ terraform/ecs_mirth.tf
@@ -48,10 +48,6 @@ resource "aws_ecs_task_definition" "mirth" {
   # host_path = "/ecs/service-storage"
   #}
 
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
-  }
 }
 
 #resource "aws_ecs_task_set" "mirth" {
