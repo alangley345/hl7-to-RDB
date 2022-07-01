1938dd36337ea54902c57d7d8a34a71c1fa2e411
Aaron Langley <alangley@gmail.com>
2022-07-01 11:30:06 -0400

    why isn't this working

--- terraform/ecs_mirth.tf
+++ terraform/ecs_mirth.tf
@@ -54,9 +54,9 @@ resource "aws_ecs_task_definition" "mirth" {
   }
 }
 
-resource "aws_ecs_task_set" "mirth" {
-  service         = aws_ecs_service.mirth.id
-  cluster         = aws_ecs_cluster.mirth.id
-  task_definition = aws_ecs_task_definition.mirth.arn
-  launch_type     = FARGATE
-}
+#resource "aws_ecs_task_set" "mirth" {
+# service         = aws_ecs_service.mirth.id
+# cluster         = aws_ecs_cluster.mirth.id
+# task_definition = aws_ecs_task_definition.mirth.arn
+# launch_type     = FARGATE
+#}
